79dc672ad4566f00b84f9c00c1943f4e34d0cfc9
Eric Butler <neptunecentury@hotmail.com>
2015-12-04 13:50:04 -0500

    Added legacy encryption methods from Beta 4 and below

---
+++ KryptPadDesktop/Extensions/Extensions.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KryptPadDesktop.Extensions
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// Converts a string to a secure string
+        /// </summary>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public static SecureString ConvertToSecureString(this string password)
+        {
+            if (password == null)
+                throw new ArgumentNullException("password");
+
+            unsafe
+            {
+                //create a fixed pointer of char array from the password string
+                fixed (char* passwordChars = password)
+                {
+                    var securePassword = new SecureString(passwordChars, password.Length);
+
+                    //prevent the secure string from being manipulated any more
+                    securePassword.MakeReadOnly();
+
+                    //return it
+                    return securePassword;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Converts a secure string to a not-so-secure string
+        /// </summary>
+        /// <param name="securePassword"></param>
+        /// <returns></returns>
+        public static string ConvertToUnsecureString(this SecureString securePassword)
+        {
+            if (securePassword == null)
+                throw new ArgumentNullException("securePassword");
+
+            //create an unmanaged char array
+            IntPtr unmanagedString = IntPtr.Zero;
+            try
+            {
+                //get the secure string as a char*
+                unmanagedString = Marshal.SecureStringToGlobalAllocUnicode(securePassword);
+
+                //return it as a string obj
+                return Marshal.PtrToStringUni(unmanagedString);
+            }
+            finally
+            {
+                //destroy it!
+                Marshal.ZeroFreeGlobalAllocUnicode(unmanagedString);
+            }
+        }
+
+    }
+}
---
+++ KryptPadDesktop/Legacy/EncryptionService.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security;
+using System.Security.Cryptography;
+using KryptPadDesktop.Extensions;
+using System.IO;
+
+namespace KryptPadDesktop.Security.Legacy
+{
+    public class EncryptionService
+    {
+        /// <summary>
+        /// Generates a cryptographic key from a password and salt
+        /// </summary>
+        /// <param name="password"></param>
+        /// <param name="saltString"></param>
+        /// <returns></returns>
+        public static byte[] GenerateEncryptionKey(SecureString password, string saltString)
+        {
+            //convert the salt string to bytes
+            byte[] salt = Convert.FromBase64String(saltString);
+
+            //derive the key from our password and salt
+            var keyGen = new Rfc2898DeriveBytes(password.ConvertToUnsecureString(), salt, 4958);
+
+            //return the first 32 bytes (256bits)
+            return keyGen.GetBytes(32);
+        }
+
+        /// <summary>
+        /// Encrypts the data using AES with a password
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public static string Encrypt(string data, SecureString password, string saltString)
+        {
+            
+            try
+            {
+                
+                using (var aes = new RijndaelManaged())
+                {
+                    
+                    //get some bytes
+                    aes.Key = GenerateEncryptionKey(password, saltString);
+                    aes.BlockSize = 256;
+                    aes.Padding = PaddingMode.PKCS7;
+
+                    //generate an IV
+                    aes.GenerateIV();
+
+                    //get the bytes for our message
+                    var plainBytes = Encoding.UTF8.GetBytes(data);
+
+                    //start up the encryption
+                    using (var ms = new MemoryStream())
+                    using (var cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        //write the bytes to the cryptostream
+                        cs.Write(plainBytes, 0, plainBytes.Length);
+                        cs.FlushFinalBlock();
+
+                        //get message bytes
+                        var msgBytes = ms.ToArray();
+
+                        //create a new array big enough for the both of 'em
+                        var cypherBytes = new byte[aes.IV.Length + msgBytes.Length];
+
+                        //return the string with the iv as the first 32 bytes. will need this when decrypting
+                        Buffer.BlockCopy(aes.IV, 0, cypherBytes, 0, aes.IV.Length);
+                        Buffer.BlockCopy(msgBytes, 0, cypherBytes, aes.IV.Length, msgBytes.Length);
+
+                        //now convert it to base64 string
+                        var cypherText = Convert.ToBase64String(cypherBytes);
+
+                        //return cypher text
+                        return cypherText;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+
+        }
+
+        /// <summary>
+        /// Decrypts data
+        /// </summary>
+        /// <param name="cypherData"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public static string Decrypt(string cypherData, SecureString password, string saltString)
+        {
+            //generate key
+            byte[] key = GenerateEncryptionKey(password, saltString);
+            //decrypt
+            return Decrypt(cypherData, key);
+        }
+        public static string Decrypt(string cypherData, byte[] key)
+        {
+            
+            try
+            {
+
+                using (var aes = new RijndaelManaged())
+                {
+
+                    //get some bytes
+                    aes.Key = key;
+                    aes.BlockSize = 256;
+                    aes.Padding = PaddingMode.PKCS7;
+
+                    //get the bytes for our message
+                    var cypherBytes = Convert.FromBase64String(cypherData);
+                    var iv = new byte[aes.IV.Length];
+                    var msgBytes = new byte[cypherBytes.Length - iv.Length];
+
+                    //we use the first 32 bytes of the cypherdata for the IV
+                    Buffer.BlockCopy(cypherBytes, 0, iv, 0, iv.Length);
+                    Buffer.BlockCopy(cypherBytes, iv.Length, msgBytes, 0, msgBytes.Length);
+
+                    //set the IV for the instance
+                    aes.IV = iv;
+
+                    //start up the decryption
+                    using (var ms = new MemoryStream())
+                    using (var cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        //write the bytes to the cryptostream
+                        cs.Write(msgBytes, 0, msgBytes.Length);
+                        cs.FlushFinalBlock();
+
+                        //the plain text has been decrypted.
+                        var plainText = System.Text.Encoding.UTF8.GetString(ms.ToArray());
+
+                        //return plain text
+                        return plainText;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+
+
+        }
+
+    }
+}
